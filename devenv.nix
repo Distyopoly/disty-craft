729faa67fe71f4406c586f28d74c295d6051d9f7
leswell <simofilevente+git@gmail.com>
2025-11-23 14:35:00 +0200

    chore: treefmt settings refactored into it's own directory

--- devenv.nix
+++ devenv.nix
@@ -16,37 +16,7 @@
     pnpm.enable = true;
   };
 
-  treefmt = {
-    enable = true;
-    config = {
-      programs = {
-        nixfmt = {
-          enable = true;
-          strict = true;
-        };
-        deadnix.enable = true;
-        # statix check highlights antipatterns in Nix code. statix fix can fix several such occurrences.
-        statix.enable = true;
-
-        # sorted things
-        keep-sorted.enable = true;
-      };
-      settings.formatter = {
-        deadnix = {
-          priority = 1;
-          options = [ "-_" ];
-        };
-
-        statix = {
-          priority = 2;
-        };
-
-        nixfmt = {
-          priority = 3;
-        };
-      };
-    };
-  };
+  treefmt = import ./nix/treefmt;
 
   processes.framework = {
     exec = "pnpm dev";
---
+++ nix/treefmt/default.nix
@@ -0,0 +1,31 @@
+{
+  enable = true;
+  config = {
+    programs = {
+      nixfmt = {
+        enable = true;
+        strict = true;
+      };
+      deadnix.enable = true;
+      # statix check highlights antipatterns in Nix code. statix fix can fix several such occurrences.
+      statix.enable = true;
+
+      # sorted things
+      keep-sorted.enable = true;
+    };
+    settings.formatter = {
+      deadnix = {
+        priority = 1;
+        options = [ "-_" ];
+      };
+
+      statix = {
+        priority = 2;
+      };
+
+      nixfmt = {
+        priority = 3;
+      };
+    };
+  };
+}
